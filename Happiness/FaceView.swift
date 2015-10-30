d688607f6284edf03d98f2cc0766a254f03b7ca9
Anthony Rosario <ajrosario08@gmail.com>
2015-10-29 17:24:20 -0700

    added generic view with UIView subclass FaceView

---
+++ Happiness/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by Anthony Rosario on 10/29/15.
+//  Copyright © 2015 Anthony Rosario. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
