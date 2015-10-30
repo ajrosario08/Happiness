95796d25b8a9d5ded53119490b9f3eeed3a0281d
Anthony Rosario <ajrosario08@gmail.com>
2015-10-29 17:18:09 -0700

    created new UIViewController subclass

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Anthony Rosario on 10/29/15.
+//  Copyright © 2015 Anthony Rosario. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
