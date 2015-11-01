42d26363a8136a07e6bc9b093ef2836b40530024
Anthony Rosario <ajrosario08@gmail.com>
2015-10-31 20:19:49 -0700

    set up model for Happiness

--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -10,4 +10,14 @@ import UIKit
 
 class HappinessViewController: UIViewController {
 
+    var happiness: Int = 50 { // 0 = very sad, 100 = ecstatic
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        
+    }
 }
