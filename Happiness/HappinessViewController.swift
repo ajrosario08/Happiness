f7e7042fd95f3fd050cd45efe32e85ea2a2aef99
Anthony Rosario <ajrosario08@gmail.com>
2015-10-31 20:41:09 -0700

    implemented HappinessViewController as FaceViewDataSource

--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class HappinessViewController: UIViewController {
+class HappinessViewController: UIViewController, FaceViewDataSource {
 
+    
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+        }
+    }
+    
     var happiness: Int = 50 { // 0 = very sad, 100 = ecstatic
         didSet {
             happiness = min(max(happiness, 0), 100)
@@ -18,6 +25,10 @@ class HappinessViewController: UIViewController {
     }
     
     func updateUI() {
-        
+        faceView.setNeedsDisplay()
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50 
     }
 }
